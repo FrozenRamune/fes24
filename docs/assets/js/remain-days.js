a0fae468f693a2c80abc8ebc32e17539d21aa175
FrozenRamune <ito.ryuma-2007.07.03@outlook.jp>
2024-08-09 23:40:43 +0900

    大体完成

--- docs/assets/js/remain-days.js
+++ docs/assets/js/remain-days.js
@@ -1,6 +1,9 @@
 const fes_start_date = new Date(2024, 9-1, 22);
 
+console.log(fes_start_date);
+
 document.addEventListener("DOMContentLoaded", (event) => {
-    const remain_days = Math.floor((fes_start_date.getTime() - Date.now()) / (24*60*60*1000));
+    const remain_days = Math.ceil((fes_start_date.getTime() - Date.now()) / (24*60*60*1000));
+    console.log(Date.now());
     document.getElementById("days-count-down").textContent = remain_days.toString();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,18 @@ import gulp_sass from "gulp-sass";
 import * as sass_compiler from 'sass';
 import pug from "gulp-pug";
 import changed from "gulp-changed";
+import request from "sync-request"
+import replace from "gulp-replace";
 
 const sass = gulp_sass(sass_compiler);
+
+function task_fetch_data() {
+  const form_answer_url = 'https://script.google.com/macros/s/AKfycbzhAHi2OMeK0q1D5ZIlKL22Isva3ImtMuIPxcvSo58_HnqwE_wsj0PVvUT8ofpf-h20/exec';
+  const clubs_str = request('GET', form_answer_url).body.toString().replaceAll('\\n', '\\\\n');
+  return gulp.src("src/_page3_data.pug")
+    .pipe(replace(/clubs_str ?=.*/, `clubs_str = '${clubs_str}';`))
+    .pipe(gulp.dest("src/"))
+}
 
 function task_copy_files() {
     return gulp.src([
@@ -23,14 +33,20 @@ function task_copy_files() {
 }
 
 function task_pug() {
-    return gulp.src(
-            "src/**/*.pug",
-            {
+    return gulp.src([
+                "src/**/*.pug",
+                "!src/**/_*.pug",
+            ], {
                 base: "src",
             }
         )
         .pipe(changed("docs"))
-        .pipe(pug())
+        .pipe(pug({
+          pretty: true,
+          locals: {
+            request,
+          }
+        }))
         .pipe(gulp.dest("docs/"))
         .pipe(browser_sync.reload({stream: true}));
 }
@@ -69,6 +85,8 @@ function task_watch() {
 }
 
 export default task_compile;
+
+export const fetch_data = task_fetch_data;
 
 export const watch =  gulp.series(
     task_compile,
